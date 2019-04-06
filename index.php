28a5972d2ca790ebe1dc81462b75d62412e799a2
Willem Monsuwe <willem@monsuwe.nl>
2019-04-06 21:43:00 +0200

    Initiele opzet

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Medicorum - work in progress</title>
+	<meta name="robots" content="noindex, nofollow">
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.4.0/css/perfect-scrollbar.min.css">
+	<link rel="stylesheet" href="css/roster.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.4.0/perfect-scrollbar.min.js"></script>
+	<script src="roster.js"></script>
+</head>
+<body>
+
+<div id="headerframe">
+    <div id="header" class="aquilafont">Pilot Roster</div>
+</div>
+<div id="main-body">
+    <div id="roster-list">
+        Loading
+    </div>
+</div>
+<div id="footerframe">
+    <div id="footer_tagline">
+        <div id= footer_logo><img src="images/logo_acme_dark_small.png" height="60px"></div>
+        <div class="aquilafont">ACME-Tech, making your job as painless as possible<br></div>
+    </div>
+    <div id="footer_corp">ACME-Tech is part of the ACME-group<br />Astro Carrera Maxima Expertis</div>
+</div>
+
+</body>
---
+++ roster.js
@@ -0,0 +1,38 @@
+$(load)
+
+var personfields = ['character_image','character_name','pilot_skill','pilot_interest']
+var skillfields = ['pilot_skill','pilot_interest']
+var specialfields = { character_image:'<img src="?">' }
+
+function load()
+{
+    $.get('getroster.php', { fields: skillfields.join(','), extrafields: 'status' }, fillroster, 'json')
+}
+
+function htmlize(text)
+{
+    return text.replace('&','&amp;').replace('<','&lt;').replace('>','&gt;').replace('"','&quot;')
+}
+
+function rosterentry(entry)
+{
+    var html = ['<div class="roster-person" data-character-id="',entry.characterID,'">']
+    for (var f = 0; f < personfields.length; f++) {
+        pf = personfields[f]
+        var text = entry[pf]
+        var sf = specialfields[pf]
+        if (sf) { text = sf.replace(/\?/, text) }
+        html.push('<div class="roster-person-',pf,'">',text,'</div>')
+    }
+    html.push('</div>')
+    return html.join('')
+}
+
+function fillroster(roster)
+{
+    var html = []
+    for (var p = 0; p < roster.people.length; p++) {
+        html.push(rosterentry(roster.people[p]))
+    }
+    $('#roster-list').html(html.join(''))
+}
