c0a0a5c4da2329338b6ab853e870a33ad486fa84
Willem Monsuwe <willem@monsuwe.nl>
2019-04-06 22:21:36 +0200

    Toevoeg-opmaak

--- roster.js
+++ roster.js
@@ -1,12 +1,14 @@
 $(load)
 
-var personfields = ['character_image','character_name','pilot_skill','pilot_interest']
-var skillfields = ['pilot_skill','pilot_interest']
-var specialfields = { character_image:'<img src="?">' }
+var person_fields = ['character_image','character_name','pilot_skill','pilot_interest']
+var skill_fields = ['pilot_skill','pilot_interest']
+var special_fields = { character_image:'<img src="?">' }
+var special_fieldsnew = { character_image:'<div class="image-add-new">+</div>' }
 
 function load()
 {
-    $.get('api/getroster.php', { fields: skillfields.join(','), extrafields: 'status' }, fillroster, 'json')
+    $.get('api/get_roster.php', { fields: skill_fields.join(','), extrafields: 'status' }, fill_roster, 'json')
+    $('#roster-list').on('click','div.roster-person.add-new',add_new_person)
 }
 
 function htmlize(text)
@@ -14,13 +16,13 @@ function htmlize(text)
     return text.replace('&','&amp;').replace('<','&lt;').replace('>','&gt;').replace('"','&quot;')
 }
 
-function rosterentry(entry)
+function roster_entry(entry)
 {
     var html = ['<div class="roster-person" data-character-id="',entry.characterID,'">']
-    for (var f = 0; f < personfields.length; f++) {
-        pf = personfields[f]
-        var text = entry[pf]
-        var sf = specialfields[pf]
+    for (var f = 0; f < person_fields.length; f++) {
+        pf = person_fields[f]
+        var text = htmlize(entry[pf])
+        var sf = special_fields[pf]
         if (sf) { text = sf.replace(/\?/, text) }
         html.push('<div class="roster-person-',pf,'">',text,'</div>')
     }
@@ -28,11 +30,28 @@ function rosterentry(entry)
     return html.join('')
 }
 
-function fillroster(roster)
+function roster_entry_new()
+{
+    var html = ['<div class="roster-person add-new">']
+    for (var f = 0; f < person_fields.length; f++) {
+        pf = person_fields[f]
+        var text = ''
+        var sf = special_fieldsnew[pf]
+        if (sf) { text = sf.replace(/\?/, text) }
+        html.push('<div class="roster-person-',pf,'">',text,'</div>')
+    }
+    html.push('</div>')
+    return html.join('')
+}
+
+function fill_roster(roster)
 {
     var html = []
     for (var p = 0; p < roster.people.length; p++) {
-        html.push(rosterentry(roster.people[p]))
+        html.push(roster_entry(roster.people[p]))
     }
+    html.push(roster_entry_new())
     $('#roster-list').html(html.join(''))
 }
+
+fuction
