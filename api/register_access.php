a13a02eadaf7650b6a5b28e6fd64b36f0e69e7de
Willem Monsuwe <willem@monsuwe.nl>
2019-07-20 15:00:35 +0200

    rfid dingen typo fixes

--- api/register_access.php
+++ api/register_access.php
@@ -1,9 +1,22 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1); 
+
+require_once 'db_sql.php';
  
+if(strcasecmp($_SERVER['REQUEST_METHOD'], 'GET') == 0){
+    echo "<html><body><h2>Access log:</h2>";
+    $result = exec_sql("SELECT fieldvalue FROM ros_fieldvalues WHERE fieldtypeID=7777");
+    while ($row = $result->fetch_assoc()) {
+        $value = $row["fieldvalue"];
+        echo "<p>Access: ${value}</p>";
+    }
+    echo "</body></html>";
+    $conn->close();
+    exit;
+}
 if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
-    throw new Exception('Request method must be POST!');
+    throw new Exception('Request method must be GET or POST!');
 }
 $contentType = trim(preg_replace("/;.*$/" ,"", isset($_SERVER["CONTENT_TYPE"]) ? $_SERVER["CONTENT_TYPE"] : ''));
 
@@ -18,13 +31,11 @@ if (strcasecmp($contentType, 'application/json') == 0){
     throw new Exception("Unsupported content type: ${contentType}");
 }
 
-require_once 'db_sql.php'
-
 $valueesc = "'".$conn->real_escape_string($cardid)." - ".$conn->real_escape_string($timestamp)."'";
 
 exec_sql("
     INSERT INTO ros_fieldvalues (fieldtypeID, characterID, prev_fieldvalueID, fieldvalue, mod_characterID)
-    VALUES (7777, 0, 0, '${valueesc}', 0)
+    VALUES (7777, 0, 0, ${valueesc}, 0)
     ");
 
 $conn->close();
