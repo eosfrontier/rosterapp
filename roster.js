ae5c5716fecfff4d200bc6a81014a11575d57b0f
Willem Monsuwe <willem@monsuwe.nl>
2020-04-15 17:03:37 +0200

    Implemented hash links

--- roster.js
+++ roster.js
@@ -318,6 +318,7 @@ function fill_one_char(person)
 
 function set_roster_type(rosid)
 {
+    if (gRosterid == rosid) return
     gRosterID = rosid
     // roster_type = $(this).attr('data-roster-type')
     // roster_label = $(this).attr('data-roster-label')
